3afb9b88587e21c8bf50e07e9619115d0b16ba8d
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-01-30 19:51:32 -0600

    Added a hover effect for the document

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.getElementById("container");
 container.style.backgroundColor="red";
 // container.style.maxWidth = "1000px";
 
+
 function createGrid (numSquare) {
 for (let i=1; i <= (numSquare**2); i++) {
 
@@ -11,7 +12,24 @@ for (let i=1; i <= (numSquare**2); i++) {
     square.style.flexBasis=`${(1/(numSquare)*100)}%`;
     square.style.flexGrow=`1`;
     container.appendChild(square);
-} 
-}
+} ;
+};
 
 createGrid(16);
+
+// const grid = document.getElementsByClassName("square");
+// console.log(gebcn);
+
+// const grid = document.querySelector(".square");
+// console.log(grid);
+
+// grid.addEventListener("mouseover", function(){
+//     // console.log(EventTarget);
+//     grid.style.backgroundColor="black";
+// });
+
+document.addEventListener('mouseover', function(e) {
+    element = e.target;
+    console.log(`element is ${element}`)
+    console.log(document.elementFromPoint(e.clientX, e.clientY)); 
+})
