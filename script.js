e167b6c985025ed4f027469b31a98539c1c08283
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-01-30 19:07:40 -0600

    Created 16x16 grid of divs using JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.getElementById("container");
+// container.style.maxWidth = "1000px";
 
-for (let i=1; i <= (16*2); i++) {
+function createGrid (numSquare) {
+for (let i=1; i <= (numSquare**2); i++) {
+
     const square = document.createElement("div");
+    square.className = "square";
     square.textContent=i;
+    square.style.flexBasis=`${(1/(numSquare)*100)}%`;
+    square.style.flexGrow=`1`;
     container.appendChild(square);
 } 
+}
+
+createGrid(16);
